4de8cff82f0b55327917d8626650e8816caf739d
Zottilenda01 <128058035+Zottilenda01@users.noreply.github.com>
2024-11-25 11:47:55 -0300

    Add files via upload

--- site de seg/main.js
+++ site de seg/main.js
@@ -2,8 +2,11 @@ const numeroSenha=document.querySelector('.parametro-senha-texto');
 let tamanhoSenha=12;
 numeroSenha.textContent=tamanhoSenha;
 const botoes=document.querySelectorAll('.parametro-senha-botao');
+const campoSenha=document.querySelector('#campo-senha');
+const letrasMaiusculas="ABCDEFGHIJKLMNOPQRSTUVWXYZÇ"
 botoes[0].onclick=aumentarTamanho;
 botoes[1].onclick=diminuirTamanho;
+geraSenha();
 function diminuirTamanho(){
     if(tamanhoSenha>=5){    tamanhoSenha=tamanhoSenha-1;
         numeroSenha.textContent=tamanhoSenha;}
@@ -11,4 +14,13 @@ function diminuirTamanho(){
 function aumentarTamanho(){
     if(tamanhoSenha<=20){    tamanhoSenha=tamanhoSenha+1;
         numeroSenha.textContent=tamanhoSenha;}
+}
+function geraSenha(){
+    let senha=" ";
+    for(let  i=0; i<tamanhoSenha;i++){
+    let numeroAleatorio=Math.random()*letrasMaiusculas.length;
+    numeroAleatorio=Math.floor(numeroAleatorio);
+    senha=senha+letrasMaiusculas[numeroAleatorio];
+}
+campoSenha.value=senha;
 }
